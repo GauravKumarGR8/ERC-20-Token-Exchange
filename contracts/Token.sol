e97b7dfa00c03aef215ff457858d3f86ba8408cc
GauravKumarGR8 <bsn.gauravkr1@gmail.com>
2023-03-19 11:47:45 +0530

    7. Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -30,8 +30,8 @@ contract Token {
 	);
 
 	constructor(
-		string memory _name, 
-		string memory _symbol, 
+		string memory _name,
+		string memory _symbol,
 		uint256 _totalSupply)
 	{
 		name = _name;
@@ -40,27 +40,33 @@ contract Token {
 		balanceOf[msg.sender] = totalSupply;
 	}
 
-	function transfer(address _to, uint256 _value) 
-		public 
+	function transfer(address _to, uint256 _value)
+		public
 		returns (bool success) 
 	{
 		// Require that sender has enough tokens to spend
 		require(balanceOf[msg.sender] >= _value);
-		require(_to != address(0));
 
-		// Deduct tokens from spender
-		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-		// Credit tokens to receiver
-		balanceOf[_to] = balanceOf[_to] + _value;
-
-		// Emit event
-		emit Transfer(msg.sender, _to, _value);
+		_transfer(msg.sender, _to, _value);
 
 		return true;
 	}
 
-	function approve(address _spender, uint256 _value) 
-		public 
+	function _transfer(
+		address _from,
+		address _to,
+		uint256 _value
+	) internal {
+		require(_to != address(0));
+
+		balanceOf[_from] = balanceOf[_from] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		emit Transfer(_from, _to, _value);
+	}
+
+	function approve(address _spender, uint256 _value)
+		public
 		returns (bool success) 
 	{
 		require(_spender != address(0));
@@ -69,5 +75,26 @@ contract Token {
 
 		emit Approval(msg.sender, _spender, _value);
 		return true;
+	}
+
+	function transferFrom(
+		address _from,
+		address _to,
+		uint256 _value
+	)
+		public
+		returns (bool success)
+	{
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+
+		// Reset Allowance
+		allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+		// spend tokens
+		_transfer(_from, _to, _value);
+
+		return true;
+
 	}
 }
