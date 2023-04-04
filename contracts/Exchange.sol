ddedd60ffc1d294f7957b26400aeea874037cd85
GauravKumarGR8 <bsn.gauravkr1@gmail.com>
2023-04-04 20:30:47 +0530

    11. Make orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,6 +8,8 @@ contract Exchange {
 	address public feeAccount;
 	uint256 public feePercent;
 	mapping(address => mapping(address => uint256)) public tokens;
+	mapping(uint256 => _Order) public orders;
+	uint256 public orderCount;
 
 	event Deposit(
 		address token,
@@ -21,9 +23,30 @@ contract Exchange {
 		address user,
 		uint256 amount,
 		uint256 balance
-	);	
+	);
 
-	constructor (address _feeAccount, uint256 _feePercent) {
+	event Order(
+		uint256 id,
+		address user,
+		address tokenGet,
+		uint256 amountGet,
+		address tokenGive,
+		uint256 amountGive,
+		uint256 timestamp
+	);
+
+	struct _Order {
+		// Attributes of an order
+		uint256 id; // Unique identifier for the order
+		address user; // User who made the order
+		address tokenGet;   // Address of the token they receive
+		uint256 amountGet;  // Amount they recieve
+		address tokenGive;  // Address of the token they give
+		uint256 amountGive; // Amount they give
+		uint256 timestamp;  // When order was created
+	}
+
+	constructor(address _feeAccount, uint256 _feePercent) {
 		feeAccount = _feeAccount;
 		feePercent = _feePercent;
 	}
@@ -62,5 +85,40 @@ contract Exchange {
 		returns(uint256)
 	{
 		return tokens[_token][_user];
+	}
+
+	// --------------------
+	// MAKE & CANCEL ORDERS
+	function makeOrder(
+		address _tokenGet,
+		uint256 _amountGet,
+		address _tokenGive,
+		uint256 _amountGive
+	) public {
+		// Prevent orders if tokens aren't on exchange
+		require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+		// Instantiate a new order
+		orderCount = orderCount + 1;
+		orders[orderCount] = _Order(
+			orderCount,
+			msg.sender,
+			_tokenGet,
+			_amountGet,
+			_tokenGive,
+			_amountGive,
+			block.timestamp
+		);
+
+		// Emit event
+		emit Order(
+			orderCount,
+			msg.sender,
+			_tokenGet,
+			_amountGet,
+			_tokenGive,
+			_amountGive,
+			block.timestamp
+		);
 	}
 }
