a0c9b72c24ea5b6c6e0e59fc4013fa1dbd3fd5d0
GauravKumarGR8 <bsn.gauravkr1@gmail.com>
2023-05-24 19:01:59 +0530

    28. My Transactions

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -41,7 +41,6 @@ export const myOpenOrdersSelector = createSelector(
 		// Fetch orders created by current account
 		orders = orders.filter((o) => o.user === account)
 
-
 		// Filter orders by selected tokens
 		orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
 		orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
@@ -168,6 +167,61 @@ const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
 	} else {
 		return RED
 	}
+}
+
+// ---------------------------------------------------------------------
+// MY FILLED ORDERS
+
+export const myFilledOrdersSelector = createSelector(
+	account,
+	tokens,
+	filledOrders,
+	(account, tokens, orders) => {
+		if (!tokens[0] || !tokens[1]) { return }
+
+		// Find our orders
+		orders = orders.filter((o) => o.user === account || o.creator === account)
+
+		// Filter orders by selected tokens
+		orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+		orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+		// Sort orders by date descending
+		orders = orders.sort((a,b) => b.timestamp - a.timestamp)
+
+		// Decorate orders - add display attributes
+		orders = decorateMyFilledOrders(orders, account, tokens)
+
+		return orders
+	}
+)
+
+const decorateMyFilledOrders = (orders, account, tokens) => {
+	return(
+		orders.map((order) => {
+			order = decorateOrder(order, tokens)
+			order = decorateMyFilledOrder(order, account, tokens)
+			return(order)
+		})
+	)
+}
+
+const decorateMyFilledOrder = (order, account, tokens) => {
+	const myOrder = order.creator === account
+
+	let orderType
+	if (myOrder) {
+		orderType = order.tokenGive === tokens[1].address ? 'buy' : 'sell'
+	} else {
+		orderType = order.tokenGive === tokens[1].address ? 'sell' : 'buy'
+	}
+
+	return ({
+		...order,
+		orderType,
+		orderClass: (orderType === 'buy' ? GREEN : RED),
+		orderSign: (orderType === 'buy' ? '+' : '-')
+	})
 }
 
 // ---------------------------------------------------------------------
